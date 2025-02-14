f1b8aea48c1c829d6fcdbafcd97270767dae0b4e
Guoying <86628141+Guoying-Li@users.noreply.github.com>
2025-02-14 14:13:44 +0100

    re

--- migrations/Version20250214130820.php
+++ migrations/Version20250214130820.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250212145256 extends AbstractMigration
+final class Version20250214130820 extends AbstractMigration
 {
     public function getDescription(): string
     {
